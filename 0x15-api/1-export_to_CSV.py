1931e18659ba4e238097abf05c06bd5f9a319b6e
Sakkie Thipane <123340673+Mphare1@users.noreply.github.com>
2023-10-23 21:14:40 +0200

    Update 1-export_to_CSV.py

--- 0x15-api/1-export_to_CSV.py
+++ 0x15-api/1-export_to_CSV.py
@@ -8,12 +8,13 @@ import csv
 from requests import get
 from sys import argv
 
-if __name__ == '__main':
+if __name__ == '__main__':
     tasks = []
     tasks_list = []
     root = 'https://jsonplaceholder.typicode.com'
-    user = get(root + '/users/{}'.format(argv[1])).json()
-    for i in get(root + '/todos').json():
+    user = get(f'{root}/users/{argv[1]}').json()
+
+    for i in get(f'{root}/todos').json():
         if i.get('userId') == int(argv[1]):
             tasks.append(argv[1])
             tasks.append(user.get('username'))
@@ -21,6 +22,7 @@ if __name__ == '__main':
             tasks.append(i.get('title'))
             tasks_list.append(tasks)
             tasks = []
-    with open('{}.csv'.format(argv[1]), 'w') as f:
+
+    with open(f'{argv[1]}.csv', 'w') as f:
         f_csv = csv.writer(f, quoting=csv.QUOTE_ALL)
         f_csv.writerows(tasks_list)
