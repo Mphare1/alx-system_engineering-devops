4b29d4688c4c65a6baf9e8dc8f90178554595567
Sakkie Thipane <123340673+Mphare1@users.noreply.github.com>
2023-10-23 21:04:52 +0200

    Update 2-export_to_JSON.py

--- 0x15-api/2-export_to_JSON.py
+++ 0x15-api/2-export_to_JSON.py
@@ -1,26 +1,29 @@
 #!/usr/bin/python3
+
 """
- exports to json file
+Exports data to a JSON file.
 """
 
-from collections import OrderedDict
 import json
 from requests import get
 from sys import argv
 
-if __name__ == '__main__':
-        task = OrderedDict()
-        tasks_list = []
-        user_dict = {}
-        root = 'https://jsonplaceholder.typicode.com'
-        user = get(root + '/users/{}'.format(argv[1])).json()
-        for i in get(root + '/todos').json():
-            if i.get('userId') == int(argv[1]):
-                task["task"] = i.get('title')
-                task['completed'] = i.get('completed')
-                task['username'] = user.get('username')
-                tasks_list.append(task)
-                task = OrderedDict()
-        user_dict[argv[1]] = tasks_list
-        with open('{}.json'.format(argv[1]), 'w') as f:
-            json.dump(user_dict, f)
+if __name__ == '__main':
+    tasks_list = []
+    user_dict = {}
+    root = 'https://jsonplaceholder.typicode.com'
+    user = get(f'{root}/users/{argv[1]}').json()
+    
+    for i in get(f'{root}/todos').json():
+        if i.get('userId') == int(argv[1]):
+            task = {
+                "task": i.get('title'),
+                "completed": i.get('completed'),
+                "username": user.get('username')
+            }
+            tasks_list.append(task)
+    
+    user_dict[argv[1]] = tasks_list
+
+    with open(f'{argv[1]}.json', 'w') as f:
+        json.dump(user_dict, f, indent=4)
