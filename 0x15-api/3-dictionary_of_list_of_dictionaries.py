16a997dd47dc0837598c554de8882169e0d48001
Sakkie Thipane <123340673+Mphare1@users.noreply.github.com>
2023-10-23 21:06:14 +0200

    Update 3-dictionary_of_list_of_dictionaries.py

--- 0x15-api/3-dictionary_of_list_of_dictionaries.py
+++ 0x15-api/3-dictionary_of_list_of_dictionaries.py
@@ -1,29 +1,34 @@
 #!/usr/bin/python3
+
 """
- exports to json
+Exports data to JSON.
 """
 
-from collections import OrderedDict
 import json
 from requests import get
 
-if __name__ == '__main__':
-        task = OrderedDict()
-        tasks_list = []
-        user_dict = {}
-        user_count = 0
-        root = 'https://jsonplaceholder.typicode.com'
-        user_id = 1
-        for i in get(root + '/todos').json():
-            if i.get('userId') != user_id:
-                tasks_list = []
-                user_id = i.get('userId')
-            user = get(root + '/users/{}'.format(i.get('userId'))).json()
-            task['username'] = user.get('username')
-            task["task"] = i.get('title')
-            task['completed'] = i.get('completed')
-            tasks_list.append(task)
-            task = OrderedDict()
-            user_dict[i.get('userId')] = tasks_list
-        with open('todo_all_employees.json', 'w') as f:
-            json.dump(user_dict, f)
+if __name__ == '__main':
+    user_dict = {}
+    root = 'https://jsonplaceholder.typicode.com'
+    user_id = None  # Initialize user_id to None
+
+    for i in get(f'{root}/todos').json():
+        current_user_id = i.get('userId')
+
+        if current_user_id != user_id:
+            # Create a new list of tasks for a different user
+            tasks_list = []
+            user_id = current_user_id
+
+        user = get(f'{root}/users/{current_user_id}').json()
+        task = {
+            "username": user.get('username'),
+            "task": i.get('title'),
+            "completed": i.get('completed')
+        }
+
+        tasks_list.append(task)
+        user_dict[current_user_id] = tasks_list
+
+    with open('todo_all_employees.json', 'w') as f:
+        json.dump(user_dict, f, indent=4)
